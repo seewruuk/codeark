b9b8f7e90879b1f6f6062183101e2840cb2e5ffc
seewruuk <sewruczekk@icloud.com>
2024-07-18 18:20:38 +0200

    remove: ''

--- app/podstrona-2/page.js
+++ app/podstrona-2/page.js
@@ -29,7 +29,7 @@ export default function Page(){
                         <Paragraph desc={
                             <>
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
-                                has been the industry's
+                                has been the industrys
                                 standard dummy text ever since the 1500s, when an unknown printer took a galley of type
                                 and scrambled it to
                                 make a type specimen book. It has survived not only five centuries, but also the leap
